844dbfa7ff974d22b599be06f0c5ca3630cfb792
Jake Morales <jmora060@ucr.edu>
2018-06-10 21:59:49 -0700

    ..

--- javaGUI/run.sh
+++ javaGUI/run.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-DBNAME="$USER"_DB
-PORT=$PGPORT
-USER=$USER
+DBNAME=$1
+PORT=$2
+USER=$3
 
 # Example: source ./run.sh jake_DB 9999 jake
 java -cp lib/*:bin/ DBproject $DBNAME $PORT $USER
